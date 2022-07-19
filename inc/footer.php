b2d2becd31cb79b770912e381f1241465ff6b284
Leonhard Holmer Patoschka <e12120510@student.tuwien.ac.at>
2022-07-19 15:50:28 +0200

    Getting info from server now :)))

--- inc/footer.php
+++ inc/footer.php
@@ -6,9 +6,10 @@
                 <li>Nonndort 6, Gars am Kamp 3571, Austria</li>
                 <li>studio@schmidt-the-art.com</li>
             </ul>
+            <a href="login.php">Login</a>
         </footer>
 
         
-        <script src="js/dropdown.js"></script>
+        <script src="js/dropdown.js">Login</script>
     </body>
 </html>
--- index.php
+++ index.php
@@ -3,15 +3,32 @@
 
 <?php 
     
-    $query = 'SELECT * FROM DRAWER';
+    $query = 'SELECT * FROM DRAWERS';
     $result = mysqli_query($conn,$query);
-    $images = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $drawers = mysqli_fetch_all($result, MYSQLI_NUM);
+    $drawers = array_map(function($drawer){return $drawer[0];}, $drawers);
 
-    $even = false; 
+
+    $images = [];
+    foreach($drawers as $drawer){
+        try{
+            $query = 'SELECT * FROM DRAWER'.$drawer;
+            $result = mysqli_query($conn, $query);
+            array_push($images,mysqli_fetch_all($result,MYSQLI_NUM));
+        }catch(Exception $e){
+            //Nothing
+        }
+    }
+
     
 ?>
 
-
+    <script>
+    let ids = <?php echo json_encode($drawers)?>;
+    let info = <?php echo json_encode($images)?>;
+    </script>
+    
+    <script src="js/index.js"></script>
     <div id="drawerSelect">
         <img src="img/symbols/arrowBack.svg" alt="Back" id="arrowBackward" class = 'arrow'>
         <h1 id="drawerTitle">Drawer 1</h1>
